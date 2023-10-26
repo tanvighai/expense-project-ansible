006940a4f48f613403c2084ab15d1052b5c03a70
tanvighai <tanvighai02@gmail.com>
2023-10-26 12:46:03 -0400

    test commit

--- memory_calculation.sh
+++ memory_calculation.sh
@@ -1,6 +1,15 @@
 total=$(free | grep Mem | awk '{ print $2 }')
 
 free=$(free | grep Mem | awk '{ print $4 }')
+used=$(free | grep Mem | awk '{ print $3 }')
 
-free-percentage=$(echo "$free / $total * 100" | bc -l | awk -F ,  '{print$1}')
-echo $free_percentage
+used-percentage=$(echo "$used / $total * 100" | bc -l | awk -F ,  '{print$1}')
+echo $used-percentage
+
+if [ $used-percentage -lt 50 ]; then
+  echo Mem is green
+elif [ $used-percentage -lt 70 ]; then
+  echo Mem is Orange
+else
+  echo Mem is Red
+fi
