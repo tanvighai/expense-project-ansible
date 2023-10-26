eb252ba19450ff185abe806ad8921d299ca16913
tanvighai <tanvighai02@gmail.com>
2023-10-26 12:20:31 -0400

    test commit

--- memory_calculation.sh
+++ memory_calculation.sh
@@ -1,6 +1,6 @@
-total=$( free | grep Mem | awk '{ print $2 }' )
+total=$(free | grep Mem | awk '{ print $2 }')
 
-free=$( free | grep Mem | awk '{ print $4 }' )
+free=$(free | grep Mem | awk '{ print $4 }')
 
 free-percentage=$(echo "$free / $total * 100" | bc -l | awk -F ,  '{print$1}')
-echo"free_percentage"
+echo $free_percentage
